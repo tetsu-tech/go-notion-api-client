dea47acc37e6f3c6f2097dba9fc8448f28bd7820
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-03-25 23:15:19 +0900

    make test method

--- client.go
+++ client.go
@@ -1,11 +1,15 @@
-package client
+package main
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 )
 
 type Client struct {
@@ -15,6 +19,29 @@ type Client struct {
 	Token string
 
 	Logger *log.Logger
+}
+
+type GetMeResponse struct {
+	Object    string      `json:"object"`
+	ID        string      `json:"id"`
+	Name      string      `json:"name"`
+	AvatarURL interface{} `json:"avatar_url"`
+	Type      string      `json:"type"`
+	Bot       struct {
+		Owner struct {
+			Type string `json:"type"`
+			User struct {
+				Object    string      `json:"object"`
+				ID        string      `json:"id"`
+				Name      string      `json:"name"`
+				AvatarURL interface{} `json:"avatar_url"`
+				Type      string      `json:"type"`
+				Person    struct {
+					Email string `json:"email"`
+				} `json:"person"`
+			} `json:"user"`
+		} `json:"owner"`
+	} `json:"bot"`
 }
 
 func NewClient(urlStr, token string, logger *log.Logger) (*Client, error) {
@@ -38,4 +65,42 @@ func NewClient(urlStr, token string, logger *log.Logger) (*Client, error) {
 		Token:      token,
 		Logger:     logger,
 	}, nil
+}
+
+func (c *Client) GetMe(ctx context.Context) (*GetMeResponse, error) {
+	reqURL := *c.URL
+
+	reqURL.Path = path.Join(reqURL.Path, "users", "me")
+
+	req, err := http.NewRequest(http.MethodGet, reqURL.String(), nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization: Bearer %s", c.Token)
+	req = req.WithContext(ctx)
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	switch res.StatusCode {
+	case http.StatusOK:
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		var getMeResponse *GetMeResponse
+		if err := json.Unmarshal(bodyBytes, &getMeResponse); err != nil {
+			return nil, err
+		}
+
+		return getMeResponse, nil
+	default:
+		return nil, errors.New("Unexpected error")
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
+
+const apiKey = ""
 
 func main() {
-	fmt.Println("Hello World")
+	notion, err := NewClient("https://api.notion.com/v1", apiKey, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := notion.GetMe(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response)
 }
