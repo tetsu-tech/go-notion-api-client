dd024ed9ba19b05eed314886dd300463905c73b3
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-05-29 19:24:27 +0900

    pass test

--- pages_test.go
+++ pages_test.go
@@ -2,9 +2,9 @@ package notion
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
-	"os"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,7 +17,6 @@ func requestRetrievePage(pageId string) (res *GetRetrievePageResponse, err error
 	}
 
 	res, err = client.GetRetrievePage(context.Background(), pageId)
-	fmt.Println(pageId)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,14 +25,70 @@ func requestRetrievePage(pageId string) (res *GetRetrievePageResponse, err error
 }
 
 func TestRetrievePage(t *testing.T) {
-	// pageId := "page1"
-	pageId := "762e592d92ca49abac6903a3c2f4c9d5"
-	// path := "https://api.notion.com/v1/pages/" + pageId
+	const resJson = `
+		{
+			"object": "page",
+			"id": "762e592d-92ca-49ab-ac69-03a3c2f4c9d5",
+			"created_time": "2022-04-09T11:29:00.000Z",
+			"last_edited_time": "2022-05-29T08:09:00.000Z",
+			"created_by": {
+					"object": "user",
+					"id": "5a8ba6f6-dda9-4256-ac73-f49b5e7cd446"
+			},
+			"last_edited_by": {
+					"object": "user",
+					"id": "fc26f6d3-1963-4097-afa0-7fd8e0d84712"
+			},
+			"cover": null,
+			"icon": null,
+			"parent": {
+					"type": "workspace",
+					"workspace": true
+			},
+			"archived": false,
+			"properties": {
+					"title": {
+							"id": "title",
+							"type": "title",
+							"title": [
+									{
+											"type": "text",
+											"text": {
+													"content": "分担＆ファイル構成",
+													"link": null
+											},
+											"annotations": {
+													"bold": false,
+													"italic": false,
+													"strikethrough": false,
+													"underline": false,
+													"code": false,
+													"color": "default"
+											},
+											"plain_text": "分担＆ファイル構成",
+											"href": null
+									}
+							]
+					}
+			},
+			"url": "https://www.notion.so/762e592d92ca49abac6903a3c2f4c9d5"
+		}
+	`
+	pageId := "page1"
+	// pageId := "762e592d92ca49abac6903a3c2f4c9d5"
+	path := "https://api.notion.com/v1/pages/" + pageId
 
 	// err := registerMock(t, resJson, path, http.MethodGet)
 	// res, err := os.ReadFile("./testdata/pages/retrieve_page.json")
 
-	resJson, err := os.ReadFile("./testdata/pages/retrieve_page.json")
+	// resJson, err := os.ReadFile("./testdata/pages/retrieve_page.json")
+	err := registerMock(t, resJson, path, http.MethodGet)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var expected *GetRetrievePageResponse
+	err = json.Unmarshal([]byte(resJson), &expected)
 
 	if err != nil {
 		log.Fatal(err)
@@ -46,7 +101,7 @@ func TestRetrievePage(t *testing.T) {
 
 	t.Run("Retrueve Page endpoint", func(t *testing.T) {
 		assert.Nil(t, err)
-		assert.Equal(t, resJson, actual)
+		assert.Equal(t, expected, actual)
 	})
 
 }
