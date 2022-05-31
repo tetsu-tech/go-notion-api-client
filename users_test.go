92d6df849f9ea8f9a3e39af93ed229206eb9ae44
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-06-01 00:09:49 +0900

    GetMeテストのリファクタリング

--- users_test.go
+++ users_test.go
@@ -6,40 +6,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"testing"
 
-	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetMe(t *testing.T) {
-	const resJson = `{"object":"user","id":"721363f4-bc34-4700-85de-c4cca6c423ad","name":"go-notion-api-client","avatar_url":null,"type":"bot","bot":{"owner":{"type":"workspace","workspace":true}}}`
-	resBytes := []byte(resJson)
-	var actual *User
-
-	err := json.Unmarshal(resBytes, &actual)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	httpmock.RegisterResponder("GET", "https://api.notion.com/v1/users/me",
-		httpmock.NewBytesResponder(200, resBytes),
-	)
-
+func requestGetMe() (res *User, err error) {
 	client, err := NewClient("token", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	res, err := client.GetMe(context.Background())
+	res, err = client.GetMe(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return res, err
+}
+
+func TestGetMe(t *testing.T) {
+	path := "https://api.notion.com/v1/users/me"
+	resJson, err := os.ReadFile("./testdata/users/get_me.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = registerMock(t, string(resJson), path, http.MethodGet)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var expected *User
+	err = json.Unmarshal([]byte(string(resJson)), &expected)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	actual, err := requestGetMe()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	t.Run("Get me endpoint", func(t *testing.T) {
 		assert.Nil(t, err)
-		assert.Equal(t, actual, res)
+		assert.Equal(t, expected, actual)
 	})
 }
 
