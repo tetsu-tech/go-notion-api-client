5ed7fdf38fb0d6978e353b7d5551e75b677903bb
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-05-31 21:42:28 +0900

    refactor test

--- pages_test.go
+++ pages_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,70 +26,22 @@ func requestRetrievePage(pageId string) (res *GetRetrievePageResponse, err error
 }
 
 func TestRetrievePage(t *testing.T) {
-	const resJson = `
-		{
-			"object": "page",
-			"id": "762e592d-92ca-49ab-ac69-03a3c2f4c9d5",
-			"created_time": "2022-04-09T11:29:00.000Z",
-			"last_edited_time": "2022-05-29T08:09:00.000Z",
-			"created_by": {
-					"object": "user",
-					"id": "5a8ba6f6-dda9-4256-ac73-f49b5e7cd446"
-			},
-			"last_edited_by": {
-					"object": "user",
-					"id": "fc26f6d3-1963-4097-afa0-7fd8e0d84712"
-			},
-			"cover": null,
-			"icon": null,
-			"parent": {
-					"type": "workspace",
-					"workspace": true
-			},
-			"archived": false,
-			"properties": {
-					"title": {
-							"id": "title",
-							"type": "title",
-							"title": [
-									{
-											"type": "text",
-											"text": {
-													"content": "分担＆ファイル構成",
-													"link": null
-											},
-											"annotations": {
-													"bold": false,
-													"italic": false,
-													"strikethrough": false,
-													"underline": false,
-													"code": false,
-													"color": "default"
-											},
-											"plain_text": "分担＆ファイル構成",
-											"href": null
-									}
-							]
-					}
-			},
-			"url": "https://www.notion.so/762e592d92ca49abac6903a3c2f4c9d5"
-		}
-	`
 	pageId := "page1"
-	// pageId := "762e592d92ca49abac6903a3c2f4c9d5"
 	path := "https://api.notion.com/v1/pages/" + pageId
 
-	// err := registerMock(t, resJson, path, http.MethodGet)
-	// res, err := os.ReadFile("./testdata/pages/retrieve_page.json")
+	res, err := os.ReadFile("./testdata/pages/retrieve_page.json")
 
-	// resJson, err := os.ReadFile("./testdata/pages/retrieve_page.json")
-	err := registerMock(t, resJson, path, http.MethodGet)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = registerMock(t, string(res), path, http.MethodGet)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var expected *GetRetrievePageResponse
-	err = json.Unmarshal([]byte(resJson), &expected)
+	err = json.Unmarshal([]byte(res), &expected)
 
 	if err != nil {
 		log.Fatal(err)
@@ -103,5 +56,4 @@ func TestRetrievePage(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, expected, actual)
 	})
-
 }
